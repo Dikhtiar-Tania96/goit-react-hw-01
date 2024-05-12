acc7cdddd743ad475c4b852b8bf3cdd205ddc2a4
Dikhtiar Tania <you@example.com>
2024-05-12 22:19:12 +0300

    add new style

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,18 @@
+import css from './Profile.module.css'
 const Profile = ({ name, tag, location, image, stats }) => {
   const { followers, views, likes } = stats;
   return (
     <>
       <div className={css.userCard}>
-        <img src={image} alt="User avatar" />
-        <div>
-          <p>{name}</p>
-          <p>@{tag}</p>
-          <p>{location}</p>
+        <img className={css.userPhoto} src={image} alt="User avatar" />
+        <div className={css.userProfile}>
+          <p className={css.userName}>{name}</p>
+          <p className={css.userText}>@{tag}</p>
+          <p className={css.userText}>{location}</p>
         </div>
 
-        <ul>
-          <li>
+        <ul className={css.userStats}>
+          <li className={css.userStatsList}>
             <span>Followers</span>
             <span>{followers}</span>
           </li>
