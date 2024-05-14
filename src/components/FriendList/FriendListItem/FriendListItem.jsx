1975d108b700952d05677674e39d325bec196d21
Dikhtiar Tania <you@example.com>
2024-05-14 18:56:59 +0300

    push second task

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,11 @@
+import css from './FriendListItem.module.css'
+
 const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
   return (
-    <li>
-      <img src={avatar} alt="Avatar" width="48" />
-      <p>{name}</p>
-      <p>{isOnline ? (<span>OnLine</span>) : (<span>OffLine</span>)}</p>
+    <li className={css.cardItem}>
+      <img className={css.photoItem} src={avatar} alt="Avatar" width="48" />
+      <p className={css.friendName}>{name}</p> 
+      <p className={css.friendStatus}>{isOnline ? (<span className={css.onLine}>OnLine</span>) : (<span className={css.offLine}>OffLine</span>)}</p>
     </li>
   );
 };
